20a2f7c54200aafb1d8c4c48a78bb3823ce83fc2
Daniel Laba <laba.daniel@gmail.com>
2019-03-14 22:23:55 +0100

    added left and right paddles with moving up/down within the window

--- Unit1.h
+++ Unit1.h
@@ -8,11 +8,28 @@
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
 #include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
         TShape *background;
+        TImage *rightPaddle;
+        TImage *leftPaddle;
+        TTimer *rightDown;
+        TTimer *rightUp;
+        TTimer *leftDown;
+        TTimer *leftUp;
+        void __fastcall rightUpTimerTimer(TObject *Sender);
+        void __fastcall rightDownTimerTimer(TObject *Sender);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormCreate(TObject *Sender);
+        void __fastcall FormResize(TObject *Sender);
+        void __fastcall leftDownTimer(TObject *Sender);
+        void __fastcall leftUpTimer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
